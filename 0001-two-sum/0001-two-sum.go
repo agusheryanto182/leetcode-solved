5dfd94b134926b149fbcb3afd184f03e090eeece
Agus Heryanto <agusheryantodev@gmail.com>
2024-04-24 08:44:39 +0700

    Time: 0 ms (100.00%), Space: 4.2 MB (60.70%) - LeetHub

--- 0001-two-sum/0001-two-sum.go
+++ 0001-two-sum/0001-two-sum.go
@@ -1,6 +1,5 @@
 func twoSum(nums []int, target int) []int {
     var dict = make(map[int]int)
-    
     for indexCurr, valCurr := range nums{
         indexDict, isPresent := dict[target - valCurr]
 
