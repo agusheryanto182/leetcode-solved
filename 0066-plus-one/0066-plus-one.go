d2e1f3a8376ce78d596ae75d6be4c8f709f130ed
Agus Heryanto <agusheryantodev@gmail.com>
2024-05-09 13:46:30 +0700

    Time: 1 ms (75.57%), Space: 2.3 MB (67.36%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.go
@@ -0,0 +1,21 @@
+func plusOne(digits []int) []int {
+	n := len(digits)
+
+	// Iterate from the least significant digit to the most significant digit
+	for i := n - 1; i >= 0; i-- {
+		// Increment the current digit by 1
+		digits[i]++
+
+		// If the digit is less than or equal to 9, no carry-over, return digits
+		if digits[i] <= 9 {
+			return digits
+		}
+
+		// Otherwise, set the current digit to 0 and continue to carry-over to the next digit
+		digits[i] = 0
+	}
+
+	// If we reach here, it means there's a carry-over to the most significant digit
+	// We need to insert a new digit 1 at the beginning of the slice
+	return append([]int{1}, digits...)
+}
