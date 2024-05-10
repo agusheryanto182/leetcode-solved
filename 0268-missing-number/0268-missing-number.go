e8629802f2e1e63fdfbc167b6d64c21d41c9c875
Agus Heryanto <agusheryantodev@gmail.com>
2024-05-10 22:18:35 +0700

    Time: 431 ms (5.80%), Space: 6.3 MB (56.89%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.go
@@ -0,0 +1,32 @@
+func missingNumber(nums []int) int {
+	if len(nums) == 1 {
+		if nums[0] != 0 {
+			return nums[0] - 1
+		}
+		return nums[0] + 1
+	}
+
+	for i := 0; i < len(nums); i++ {
+		for j := i; j < len(nums); j++ {
+			if nums[i] > nums[j] {
+				temp := nums[i]
+				nums[i] = nums[j]
+				nums[j] = temp
+			}
+		}
+	}
+
+	fmt.Println(nums)
+
+	temp := 0
+	for k := 0; k < len(nums)-1; k++ {
+		if nums[0] != 0 {
+			return nums[0] - 1
+		}
+		if nums[k]+1 != nums[k+1] {
+			return nums[k] + 1
+		}
+		temp = nums[k+1] + 1
+	}
+	return temp
+}
