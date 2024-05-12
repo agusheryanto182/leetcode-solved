6e75fc651fae9a2116a33e92e73b3b7084915d79
Agus Heryanto <agusheryantodev@gmail.com>
2024-05-12 10:18:37 +0700

    Time: 15 ms (36.65%), Space: 6.1 MB (87.53%) - LeetHub

--- 0136-single-number/0136-single-number.go
+++ 0136-single-number/0136-single-number.go
@@ -1,17 +1,9 @@
 func singleNumber(nums []int) int {
-	occurrence := make(map[int]int)
-
-	// Menghitung kemunculan setiap angka
-	for _, num := range nums {
-		occurrence[num]++
-	}
-
-	// Mencari angka tunggal
-	for num, count := range occurrence {
-		if count == 1 {
-			return num
-		}
-	}
-
-	return 0 // Jika tidak ada angka tunggal, maka mengembalikan 0
+    var result int
+    
+    for _, n := range nums {
+        result ^= n
+    }
+    
+    return result
 }
