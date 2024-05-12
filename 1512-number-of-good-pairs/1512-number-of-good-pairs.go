2f125483b60383b11216a2b4b416e64a2a31d556
Agus Heryanto <agusheryantodev@gmail.com>
2024-05-12 08:05:25 +0700

    Time: 1 ms (79.49%), Space: 2.3 MB (45.70%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.go
@@ -0,0 +1,11 @@
+func numIdenticalPairs(nums []int) int {
+    counter := 0
+    for i := 0; i < len(nums); i++{
+        for j:=i; j < len(nums); j++{
+            if nums[i] == nums[j] && i < j{
+                counter++
+            }
+        }
+    }
+    return counter
+}
