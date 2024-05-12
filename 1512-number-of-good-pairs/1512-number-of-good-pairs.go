81de42f99bd74b38b5186e2ccf154209effdc953
Agus Heryanto <agusheryantodev@gmail.com>
2024-05-12 08:43:48 +0700

    Time: 0 ms (100.00%), Space: 2.2 MB (45.70%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.go
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.go
@@ -1,19 +1,11 @@
 func numIdenticalPairs(nums []int) int {
-	counter := 0
-	temp := make(map[int]int)
-	for t, numb := range nums {
-		temp[t] = numb
-	}
-
-
-	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i] == temp[j] {
-				counter++
-			} else if nums[i] == nums[j] {
-				counter++
-			}
-		}
-	}
-	return counter
+    counter := 0
+    for i := 0; i < len(nums); i++{
+        for j:=i; j < len(nums); j++{
+            if nums[i] == nums[j] && i < j{
+                counter++
+            }
+        }
+    }
+    return counter
 }
