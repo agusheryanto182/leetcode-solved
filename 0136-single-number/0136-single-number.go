11e3041bfe18af9fdada3eefa835597cfa6afa45
Agus Heryanto <agusheryantodev@gmail.com>
2024-05-12 10:45:01 +0700

    Time: 8 ms (93.98%), Space: 6.4 MB (41.60%) - LeetHub

--- 0136-single-number/0136-single-number.go
+++ 0136-single-number/0136-single-number.go
@@ -1,9 +1,16 @@
+
 func singleNumber(nums []int) int {
-    var result int
-    
-    for _, n := range nums {
-        result ^= n
-    }
-    
-    return result
+	// 1, 2, 3, 2, 1
+	// 3:  0 1 1
+	// 1:  0 0 1
+	// ------
+	// XOR 0 1 0
+
+	var result int
+
+	for _, n := range nums {
+		result = result ^ n
+	}
+
+	return result
 }
