104384b29203f1dcc7f531f809f870b5cc1f8f03
Agus Heryanto <agusheryantodev@gmail.com>
2024-05-12 10:14:10 +0700

    Time: 316 ms (5.01%), Space: 6.4 MB (29.82%) - LeetHub

--- 0136-single-number/0136-single-number.go
+++ 0136-single-number/0136-single-number.go
@@ -1,17 +1,13 @@
 func singleNumber(nums []int) int {
-	temp := make(map[int]int)
 	for i, numb := range nums {
 		for j := 0; j < len(nums); j++ {
 			if numb == nums[j] && i != j {
-				temp[numb] = i
+				break
 			}
-		}
-	}
 
-	for _, char := range nums {
-		_, ok := temp[char]
-		if !ok {
-			return char
+			if j == len(nums)-1 {
+				return numb
+			}
 		}
 	}
 	return 0
