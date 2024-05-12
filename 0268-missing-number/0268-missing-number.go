1edd894392e4c9ab800404bd4a95271ea5806825
Agus Heryanto <agusheryantodev@gmail.com>
2024-05-12 10:47:08 +0700

    Time: 4 ms (99.32%), Space: 6.4 MB (40.32%) - LeetHub

--- 0268-missing-number/0268-missing-number.go
+++ 0268-missing-number/0268-missing-number.go
@@ -1,30 +1,40 @@
+// func missingNumber(nums []int) int {
+// 	if len(nums) == 1 {
+// 		if nums[0] != 0 {
+// 			return nums[0] - 1
+// 		}
+// 		return nums[0] + 1
+// 	}
+
+// 	for i := 0; i < len(nums); i++ {
+// 		for j := i; j < len(nums); j++ {
+// 			if nums[i] > nums[j] {
+// 				temp := nums[i]
+// 				nums[i] = nums[j]
+// 				nums[j] = temp
+// 			}
+// 		}
+// 	}
+
+// 	temp := 0
+// 	for k := 0; k < len(nums)-1; k++ {
+// 		if nums[0] != 0 {
+// 			return nums[0] - 1
+// 		}
+// 		if nums[k]+1 != nums[k+1] {
+// 			return nums[k] + 1
+// 		}
+// 		temp = nums[k+1] + 1
+// 	}
+// 	return temp
+// }
+
+
 func missingNumber(nums []int) int {
-	if len(nums) == 1 {
-		if nums[0] != 0 {
-			return nums[0] - 1
-		}
-		return nums[0] + 1
-	}
-
-	for i := 0; i < len(nums); i++ {
-		for j := i; j < len(nums); j++ {
-			if nums[i] > nums[j] {
-				temp := nums[i]
-				nums[i] = nums[j]
-				nums[j] = temp
-			}
-		}
-	}
-
-	temp := 0
-	for k := 0; k < len(nums)-1; k++ {
-		if nums[0] != 0 {
-			return nums[0] - 1
-		}
-		if nums[k]+1 != nums[k+1] {
-			return nums[k] + 1
-		}
-		temp = nums[k+1] + 1
-	}
-	return temp
+    var currSum, fullSum int
+    for i := 0; i < len(nums); i++{
+        currSum += nums[i]
+        fullSum += i
+    }
+    return fullSum + len(nums) - currSum;
 }
