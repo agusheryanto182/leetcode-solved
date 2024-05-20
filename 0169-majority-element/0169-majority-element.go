ac6439205eb0867f44bae499de8133a00001157e
Agus Heryanto <agusheryantodev@gmail.com>
2024-05-20 08:13:17 +0700

    Time: 25 ms (11.77%), Space: 6.9 MB (31.98%) - LeetHub

--- 0169-majority-element/0169-majority-element.go
+++ 0169-majority-element/0169-majority-element.go
@@ -1,17 +1,33 @@
-func majorityElement(nums []int) int {
-	dict := make(map[int]int)
-	for i := 0; i < len(nums); i++ {
-		dict[nums[i]]++
-	}
+// func majorityElement(nums []int) int {
+// 	dict := make(map[int]int)
+// 	for i := 0; i < len(nums); i++ {
+// 		dict[nums[i]]++
+// 	}
 
-	result := 0
-	temp := 0
-	for _, numb := range nums {
-		if dict[numb] > temp {
-			temp = dict[numb]
-			result = numb
+// 	result := 0
+// 	temp := 0
+// 	for _, numb := range nums {
+// 		if dict[numb] > temp {
+// 			temp = dict[numb]
+// 			result = numb
+// 		}
+// 	}
+
+// 	return result
+// }
+
+func majorityElement(nums []int) int {
+	var majNum, count int
+	for _, num := range nums {
+		if count == 0 {
+			majNum = num
+		}
+
+		if num == majNum {
+			count++
+		} else {
+			count--
 		}
 	}
-
-	return result
+	return majNum
 }
