397ac6106722c2a8610fa9d00c34dd2baed32d35
Agus Heryanto <agusheryantodev@gmail.com>
2024-05-20 10:00:07 +0700

    Time: 16 ms (74.13%), Space: 7 MB (55.08%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.go
+++ 0283-move-zeroes/0283-move-zeroes.go
@@ -1,16 +1,30 @@
-func moveZeroes(nums []int) {
-    lastNonZeroFoundAt := 0
+// func moveZeroes(nums []int) {
+//     lastNonZeroFoundAt := 0
     
-    // Move all non-zero elements to the front of the array
-    for i := 0; i < len(nums); i++ {
-        if nums[i] != 0 {
-            nums[lastNonZeroFoundAt] = nums[i]
-            lastNonZeroFoundAt++
+//     // Move all non-zero elements to the front of the array
+//     for i := 0; i < len(nums); i++ {
+//         if nums[i] != 0 {
+//             nums[lastNonZeroFoundAt] = nums[i]
+//             lastNonZeroFoundAt++
+//         }
+//     }
+    
+//     // Fill the remaining positions with zeroes
+//     for i := lastNonZeroFoundAt; i < len(nums); i++ {
+//         nums[i] = 0
+//     }
+// }
+
+func moveZeroes(nums []int)  {
+    newIdx := 0
+    for _, val := range nums {
+        if val != 0 {
+            nums[newIdx] = val
+            newIdx = newIdx + 1
         }
     }
-    
-    // Fill the remaining positions with zeroes
-    for i := lastNonZeroFoundAt; i < len(nums); i++ {
-        nums[i] = 0
-    }
+    for newIdx < len(nums) {
+        nums[newIdx] = 0
+        newIdx = newIdx + 1
+        }
 }
