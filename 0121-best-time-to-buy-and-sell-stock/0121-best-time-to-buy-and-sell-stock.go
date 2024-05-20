7625b4b6f74e7972bb08563f022f82e575b497ef
Agus Heryanto <agusheryantodev@gmail.com>
2024-05-20 09:23:23 +0700

    Time: 171 ms (5.08%), Space: 12.1 MB (8.10%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.go
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.go
@@ -1,21 +1,37 @@
+// func maxProfit(prices []int) int {
+// 	if len(prices) == 0 {
+// 		return 0
+// 	}
+
+// 	minPrice := prices[0]
+// 	maxProfit := 0
+
+// 	for i := 1; i < len(prices); i++ {
+// 		if prices[i] < minPrice {
+// 			minPrice = prices[i]
+// 		} else {
+// 			profit := prices[i] - minPrice
+// 			if profit > maxProfit {
+// 				maxProfit = profit
+// 			}
+// 		}
+// 	}
+
+// 	return maxProfit
+// }
+
 func maxProfit(prices []int) int {
-	if len(prices) == 0 {
-		return 0
-	}
+    left, right, profit, result := 0, 1, 0, 0
 
-	minPrice := prices[0]
-	maxProfit := 0
+    for right < len(prices) {
+        if prices[left] < prices[right] {
+            profit = prices[right] - prices[left]
+            result = max(result, profit)
+        } else {
+            left = right
+        }
+        right++
+    }
 
-	for i := 1; i < len(prices); i++ {
-		if prices[i] < minPrice {
-			minPrice = prices[i]
-		} else {
-			profit := prices[i] - minPrice
-			if profit > maxProfit {
-				maxProfit = profit
-			}
-		}
-	}
-
-	return maxProfit
+    return result
 }
