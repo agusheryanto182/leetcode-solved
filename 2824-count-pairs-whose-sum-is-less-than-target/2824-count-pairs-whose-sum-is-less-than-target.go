32a88b567e1b2844e7ca92df475d905fbee18d22
Agus Heryanto <agusheryantodev@gmail.com>
2024-05-21 11:29:17 +0700

    Time: 5 ms (38.86%), Space: 2.7 MB (100.00%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.go
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.go
@@ -1,12 +1,30 @@
-func countPairs(nums []int, target int) int {
-	result := 0
+// func countPairs(nums []int, target int) int {
+// 	result := 0
 
-	for i := 0; i < len(nums)-1; i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i]+nums[j] < target {
-				result++
-			}
-		}
-	}
-	return result
+// 	for i := 0; i < len(nums)-1; i++ {
+// 		for j := i + 1; j < len(nums); j++ {
+// 			if nums[i]+nums[j] < target {
+// 				result++
+// 			}
+// 		}
+// 	}
+// 	return result
+// }
+
+func countPairs(nums []int, target int) int {
+    count := 0
+    sort.Ints(nums)
+
+    left, right := 0, len(nums) - 1
+
+    for left < right {
+        if nums[left] + nums[right] < target {
+            count += right - left
+            left ++
+        } else {
+            right --
+        }
+    }
+
+    return count
 }
