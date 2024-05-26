d838213311993be73aca2939be67761e50ec4fbd
Agus Heryanto <agusheryantodev@gmail.com>
2024-05-26 21:00:58 +0700

    Time: 2 ms (80.53%), Space: 3.7 MB (76.70%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.go
+++ 1470-shuffle-the-array/1470-shuffle-the-array.go
@@ -1,13 +1,22 @@
-func shuffle(nums []int, n int) []int {
-	result := []int{}
-	numsx := []int{}
-	numsy := []int{}
-	numsx = append(numsx, nums[:len(nums)/2]...)
-	numsy = append(numsy, nums[len(nums)/2:]...)
+// func shuffle(nums []int, n int) []int {
+// 	result := []int{}
+// 	numsx := []int{}
+// 	numsy := []int{}
+// 	numsx = append(numsx, nums[:len(nums)/2]...)
+// 	numsy = append(numsy, nums[len(nums)/2:]...)
 
-	for i := 0; i < len(nums)/2; i++ {
-		result = append(result, numsx[i])
-		result = append(result, numsy[i])
-	}
-	return result
+// 	for i := 0; i < len(nums)/2; i++ {
+// 		result = append(result, numsx[i])
+// 		result = append(result, numsy[i])
+// 	}
+// 	return result
+// }
+
+func shuffle(nums []int, n int) []int {
+    result := make([]int,2*n)
+    for i:=0;i<n;i++{
+        result[i*2] = nums[i]
+        result[i*2+1] = nums[i+n]
+    }
+    return result
 }
