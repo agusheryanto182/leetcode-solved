9f195e255ced96cf8fa8f5b6d21cf98132578582
Agus Heryanto <agusheryantodev@gmail.com>
2024-05-29 22:44:41 +0700

    Time: 5 ms (36.00%), Space: 2.7 MB (71.43%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.go
@@ -0,0 +1,9 @@
+func numberOfEmployeesWhoMetTarget(hours []int, target int) int {
+    counter := 0
+    for _, val := range hours{
+        if val >= target {
+            counter ++
+        }
+    }
+    return counter
+}
