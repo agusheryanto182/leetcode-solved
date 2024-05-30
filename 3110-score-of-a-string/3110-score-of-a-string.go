6328750de912c2294ddc882079d782a9227fad5d
Agus Heryanto <agusheryantodev@gmail.com>
2024-05-30 08:23:56 +0700

    Time: 0 ms (100.00%), Space: 2.3 MB (90.98%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.go
+++ 3110-score-of-a-string/3110-score-of-a-string.go
@@ -2,8 +2,8 @@ func scoreOfString(s string) int {
 	count := 0
 
 	for i := 0; i < len(s)-1; i++ {
-		curr := int(math.Abs(float64((int(s[i]) - int(s[i+1])))))
-		count += curr
+		curr := int(s[i]) - int(s[i+1])
+		count += int(math.Abs(float64(curr)))
 	}
 	return count
 }
